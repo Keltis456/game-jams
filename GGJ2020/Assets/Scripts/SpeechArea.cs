e6d360ccc8ab4ea32bd4ef97fafb2491ac26324c
Nikita Brik <keltis.kk@gmail.com>
2020-02-01 05:49:25 +0200

    SpeechArea

---
+++ GGJ2020/Assets/Scripts/SpeechArea.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SpeechArea : MonoBehaviour
+{
+    [TextArea]
+    [SerializeField] private string text;
+
+    [SerializeField] private GameObject backImage;
+    private bool isShown;
+
+    private void Start()
+    {
+        backImage.SetActive(false);
+
+        GetComponentInChildren<TMP_Text>().text = "";
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other);
+        if (other.CompareTag("Player") && !isShown)
+        {
+            Debug.Log("Player");
+            //backImage.SetActive(true);
+            isShown = true;
+            StartCoroutine(TypeText(GetComponentInChildren<TMP_Text>()));
+        }
+    }
+
+    private IEnumerator TypeText(TMP_Text tmpText)
+    {
+        foreach (var letter in text)
+        {
+            tmpText.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
